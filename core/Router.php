b487e767a63c5288b6ffb5d8123f0c1d19567688
Luc Fujitsu <luc1024@gmail.com>
2022-05-07 22:06:10 +0300

    // 29:06

--- core/Router.php
+++ core/Router.php
@@ -5,15 +5,7 @@ namespace app\core;
 class Router
 {
     public Request $request;
-    protected array $routes = [
-        'get' => [
-            '/' => callback,
-            '/contact' => callback,
-        ],
-        'post' => [
-            '' => callback,
-        ]
-    ];
+    protected array $routes = [];
 
     /**
      * @param Request $request
@@ -29,14 +21,16 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+
     public function resolve()
         {
             $path = $this->request->getPath();
             $method = $this->request->getMethod();
             $callback = $this->routes[$method][$path] ?? false;
             if ($callback === false){
-                echo "404 Not found";
+                echo "404 Not found!!!";
                 exit;
             }
+            echo call_user_func($callback);
         }
 }
