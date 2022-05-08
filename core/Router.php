8eddb4e89702caee0d3ae5eccfed5d46171d681e
Luc Fujitsu <luc1024@gmail.com>
2022-05-08 11:27:21 +0300

    //43:31

--- core/Router.php
+++ core/Router.php
@@ -34,6 +34,28 @@ class Router
             if (is_string($callback)){
                 return $this->renderView($callback);
             }
-            echo call_user_func($callback);
+            return call_user_func($callback);
         }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
+    }
 }
